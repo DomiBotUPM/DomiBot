79b0400dfae870a2948bc7475327401282f88e4e
Jean Paul <jeanpaul10@live.com>
2023-04-10 17:05:18 +0200

    Añadida versión que detecta imagen girada

--- vision/main.py
+++ vision/main.py
@@ -1,15 +1,22 @@
 import os
 import cv2 as cv
-from opencv.piece_recognition_v1 import piece_recognition
+from opencv.piece_recognition_v2 import piece_recognition
 
-# path_dir = "dataset_fichas/"
-# filename = os.path.join(path_dir, "5x4/20180505_181205.jpg")
-# img = cv.imread(filename)
+path_dir = "dataset_fichas/"
+filename = os.path.join(path_dir, "6x5/20180505_181840.jpg")
+img = cv.imread(filename)
 
-capture = cv.VideoCapture(0)
-_, img = capture.read()
-capture.release
-print(img.shape)
+# Capura a partir de la cámara
+# capture = cv.VideoCapture(0)
+# _, img = capture.read()
+# while True:
+#     _, frame = capture.read()
+#     cv.imshow('Video', frame)
+
+#     if cv.waitKey(20) & 0xFF==ord('d'):
+#         break
+# capture.release
+
 type_piece = piece_recognition(img)
 print(f"La ficha es de tipo {type_piece}")
 
