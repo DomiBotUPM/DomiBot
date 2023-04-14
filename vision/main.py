4818fa5b8def7e6450073daa19aa315aacdbc2a9
Jean Paul <jeanpaul10@live.com>
2023-04-14 20:23:55 +0200

    Modificación del main del módulo de visión para poder realizar pruebas de forma más sencilla

--- vision/main.py
+++ vision/main.py
@@ -1,31 +1,56 @@
 import os
 import cv2 as cv
-from opencv.piece_recognition_v2 import piece_recognition
+from opencv.pieces_recognition_v1 import pieces_recognition
+from opencv.pieces_detection_v1 import pieces_detection
 import time
+from datetime import datetime
 
-# path_dir = "dataset_fichas/"
-# filename = os.path.join(path_dir, "6x5/20180505_181840.jpg")
-# img = cv.imread(filename)
 
-# Capura a partir de la cámara
-capture = cv.VideoCapture(0)
+# tamaño fichas: 3.8x1.9 cm
 
-width = capture.get(cv.CAP_PROP_FRAME_WIDTH)
-height = capture.get(cv.CAP_PROP_FRAME_HEIGHT)
-
-while True:
-    _, frame = capture.read()
-    cv.imshow('Video', frame)
-    success, type_piece = piece_recognition(frame, size=width*height)
-    if success:
-        print(f"La ficha es de tipo {type_piece}")
+def test_with_image(filename):
+    img = cv.imread(filename)
+    cv.imshow("Imagen real", img)
+    recognitions = pieces_recognition(img, size=img.shape[0]*img.shape[1], preprocess=True, verbose=False, visualize=True)
+    # detections = pieces_detection(img, size=img.shape[0]*img.shape[1], preprocess=True, verbose=False, visualize=True)
+    if len(recognitions):
+        print(f"Se han reconocido {len(recognitions)} piezas")
+        # print(recognitions)
     else:
-        print(f"No se ha podido reconocer la pieza")
-    time.sleep(2)
-    if cv.waitKey(20) & 0xFF==ord('d'):
-        break
-capture.release
+        print(f"No se ha podido reconocer ninguna pieza")
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+def test_with_video(channel=1):
+    # Capura a partir de la cámara
+    capture = cv.VideoCapture(1)
+
+    width = capture.get(cv.CAP_PROP_FRAME_WIDTH)
+    height = capture.get(cv.CAP_PROP_FRAME_HEIGHT)
+
+    while True:
+        ret, frame = capture.read()
+        cv.imshow('Video', frame)
+        recognitions = pieces_recognition(frame, size=width*height, verbose=False, visualize=True)
+        # detections = pieces_detection(img, size=img.shape[0]*img.shape[1], preprocess=True, verbose=False, visualize=True)
+        if len(recognitions):
+            print(f"Se han reconocido {len(recognitions)} piezas")
+            print(recognitions)
+        else:
+            print(f"No se ha podido reconocer ninguna pieza")
+        time.sleep(0.2)
+        if cv.waitKey(20) & 0xFF==ord('d'):
+            break
+    capture.release
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+    
+def save_img(img):
+    now = datetime.now()
+    filename_dest = "fotos_ur3/" + now.strftime("%Y%m%d_%H%M%S") + ".jpg"
+    cv.imwrite(filename_dest, img)
 
 
-
-cv.destroyAllWindows()
+path_dir = "fotos_ur3/"
+filename = os.path.join(path_dir, "20230412_194329.jpg")
+test_with_image(filename)
