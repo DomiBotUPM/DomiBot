0b49c939ea8ef6f6351c515545c167f3acce364a
Jean Paul <jeanpaul10@live.com>
2023-04-30 21:19:00 +0200

    Se añade un fichero de ejemplo del uso de este paquete de vision

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+from vision.vision_interface import DominoVision
+import cv2 as cv
+
+domino_vision = DominoVision(visualize=True, verbose=False)
+
+# Probar directamente desde la cámara
+# domino_vision.test_with_video(channel=1)
+
+# Probar con imagenes
+path_dir = os.path.abspath("vision/fotos_ur3/")
+i = 0
+for file in os.listdir(path_dir)[:]:
+    filename = os.path.join(path_dir, file)
+    domino_vision.test_with_image(filename)
+    cv.waitKey(0)
+    
